5035c708ebf6b59661f3085bab6b996c38732ec2
zaayush <148395460+zaayush@users.noreply.github.com>
2023-11-07 23:01:08 -0800

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,20 +1,56 @@
-# Function to check if any player has won
-def check_win(player_pos, cur_player):
- 
-    # All possible winning combinations
-    soln = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
- 
-    # Loop to check if any winning combination is satisfied
-    for x in soln:
-        if all(y in player_pos[cur_player] for y in x):
- 
-            # Return True if any winning combination satisfies
-            return True
-    # Return False if no combination is satisfied       
-    return False       
- 
-# Function to check if the game is drawn
-def check_draw(player_pos):
-    if len(player_pos['X']) + len(player_pos['O']) == 9:
-        return True
-    return False       
+def check_winner(board):
+    # check rows
+    #    ['X', 'X', 'X'], -> set(['X', 'X', 'X']) -> {'X'}
+    #    ['O', 'X', 'O'], -> set(['O', 'X', 'O']) -> {'X', 'O'}
+    #    ['O', 'O', 'X'],
+    for row in board:
+        if len(set(row)) == 1:
+            return row[0]
+
+    # check columns
+    #    ['X', 'X', 'X'],
+    #    ['O', 'X', 'O'],
+    #    ['O', 'O', 'X'],
+    # how to index -> board[row][column]
+    # column_idxs = [[0,0], [1,0], [2,0]]
+    # column_idxs = [[0,1], [1,1], [2,1]]
+    for i in range(len(board)):
+        # len(board) -> 3
+        column = [board[j][i] for j in range(len(board))]
+        # column => ['X', 'O', 'O']
+        # column => ['X', 'X', 'O]
+        if len(set(column)) == 1:
+            return board[0][i]
+
+    # check diagonals
+    # check columns
+    #    ['X', 'X', 'X'],
+    #    ['O', 'X', 'O'],
+    #    ['O', 'O', 'X'],
+    # how to index -> board[row][column]
+    # idx -> [[0,0], [1,1], [2, 2]]
+    top_left_to_bottom_right = [board[i][i] for i in range(len(board))]
+    if len(set(top_left_to_bottom_right)) == 1:
+        return board[0][0]
+
+    # check diagonals
+    # check columns
+    #    ['X', 'X', 'X'],
+    #    ['O', 'X', 'O'],
+    #    ['O', 'O', 'X'],
+    # how to index -> board[row][column]
+    # idx -> [[0,2], [1,1], [2, 0]]
+    top_right_to_bottom_left = [board[i][len(board)-i-1] for i in range(len(board))]
+    if len(set(top_right_to_bottom_left)) == 1:
+        return board[0][len(board)-1]
+    
+    
+    flat_board = []
+    for row in board:
+        flat_board.extend(row)
+
+    if not None in flat_board:
+        return"draw"
+    return None
+    return "Draw"
+
