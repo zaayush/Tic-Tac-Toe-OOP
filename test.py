5a3f7eca05b5ebd0c2e7913258e7c4ec08d15f79
zaayush <148395460+zaayush@users.noreply.github.com>
2023-11-08 00:32:35 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-# Test Scenarios
+from logic import check_winner
+
+boards = [
+    [
+        ['O', '1', 'O'],
+        ['O', '1', 'O'],
+        ['O', 'O', '1'],
+    ],
+    [
+        [None, None, None],
+        [None, None, None],
+        [None, None, None],
+    ],
+    [
+        ['X', 'O', 'X'],
+        ['O', 'X', 'X'],
+        ['O', 'X', 'O'],
+    ],
+]
+
+for idx, board in enumerate(boards):
+    winner = check_winner(board)
+    print(f"{idx}: {winner}")# Test Scenarios
